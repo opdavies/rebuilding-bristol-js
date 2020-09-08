ee7f0d578e07f662978d3f7d57fbd5ae99cf40ad
Oliver Davies <oliver@oliverdavies.uk>
2020-09-08 20:17:02 +0100

    Override default colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   purge: [],
   theme: {
+    colors: {
+      black: '#222',
+      blue: '#204d74',
+      white: '#fff',
+      yellow: '#fde546'
+    },
     extend: {},
   },
   variants: {},
