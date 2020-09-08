8e07b4da45accbc4d7e8c8f6c4a7ecb8da2a723e
Oliver Davies <oliver@oliverdavies.uk>
2020-09-08 19:45:02 +0100

    Remove the HelloWorld component and default logo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{ title }}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  data() {
+    return {
+      title: 'Bristol JS'
+    }
   }
 }
 </script>
